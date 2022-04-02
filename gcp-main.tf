a3d13599eece7277a28877b84138b8372802fc5c
Fernando Freitas <fsf.freitas@hotmail.com>
2022-04-01 21:14:36 -0300

    Teste

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,9 +1,9 @@
-resource "google_app_engine_application" "app" {
+resource "google_app_engine_application" "my-app-6aso-fer" {
   project     = "labdevopscloud-6aso-fer"
   location_id = "us-central"
 }
 
-resource "google_artifact_registry_repository" "my-repo" {
+resource "google_artifact_registry_repository" "my-repo-6aso-fer" {
   provider = google-beta
 
   location = "us-central1"
